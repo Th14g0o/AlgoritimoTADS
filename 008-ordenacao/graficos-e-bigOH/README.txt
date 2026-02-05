3879f06595944517c57f10def070ebfc82a2099a
Thiago Gonsalves <geekland135@gmail.com>
2026-02-04 23:15:59 -0300

    graficos gerados e notação big oh

---
+++ 008-ordenacao/graficos-e-bigOH/README.txt
@@ -0,0 +1,92 @@
+Algoritimo de ordenação com seleção
+
+BigOH = O(t²)
+
+[ CODIGO ABAIXO ]
+void selecao(int a[], int t){
+    int i, j; 
+    int min, aux;
+    for (i = 0; i < (t-1); i++){
+        min = i;
+        for (j = (i+1); j < t; j++) {
+            if(a[j] < a[min])
+            min = j;
+        }
+        if (a[i] != a[min]) {
+            aux = a[i];
+            a[i] = a[min];
+            a[min] = aux;
+        }
+    }
+}
+[ CODIGO ACIMA ]
+
+________________________________________________________
+
+Algoritimo de ordenação com inserção
+
+BigOH = O(t²)
+
+[ CODIGO ABAIXO ]
+void insercao(int a[], int t){
+    int i,x;
+    for (i=0 ; i<t ; ++i){
+        int key = a[i];
+        x = i - 1;
+        while (x >= 0 && a[x] > key) {
+            a[x + 1] = a[x];
+            x = x - 1;
+        }
+        a[x + 1] = key;
+    }
+}
+[ CODIGO ACIMA ]
+
+________________________________________________________
+
+Algoritimo de ordenação com merge-sort
+
+BigOH = O((n log n))
+
+[ CODIGO ABAIXO ]
+void merge(int a[], int i1, int j1, int i2, int j2) {
+    int* temp = new int[((j1 - i1) + (j2 - i2) + 2)];
+    int i, j, k;
+    i = i1;
+    j = i2;
+    k = 0;
+    while (i <= j1 && j <= j2) {
+        if (a[i] < a[j])
+            temp[k++] = a[i++];
+        else
+            temp[k++] = a[j++];
+    }
+    while (i <= j1)
+        temp[k++] = a[i++];
+    while (j <= j2)
+        temp[k++] = a[j++];
+    for (i = i1, j = 0;i <= j2;i++, j++)
+        a[i] = temp[j];
+    delete[] temp;
+}
+
+void merge_sort(int a[], int primeiro, int ultimo){
+    int meio;
+    if (primeiro < ultimo) {
+        meio = (primeiro + ultimo) / 2;
+        merge_sort(a, primeiro, meio);
+        merge_sort(a, meio + 1, ultimo);
+        merge(a, primeiro, meio, meio + 1, ultimo);
+    }
+};
+[ CODIGO ACIMA ]
+
+________________________________________________________
+
+Melhor escolha
+
+O merge-sort é o mais eficiente em todos os casos testados
+
+> Nos casos 3 e 5 a seleção é mais eficiente que o de inserção
+> nos casos 2, 4 e 1 o insert é mais eficiente que o de seleção
+> No caso 2 a inserção e o merge tiveram valores tão proximos que a barra do insert e desenhado embaixo.
--- grafico_python.py
+++ grafico_python.py
@@ -2,50 +2,68 @@ import os
 import matplotlib.pyplot as plt
 import pandas as pd
 
+class gera_grafico:
+    def __init__(self, caminho = ""):
+        self.pasta = caminho
+        self.tipos = ['i', 'm', 's']
+
+    def gerar(self, caminho = "", titulo = "Grafico"):
+        if (caminho != ""):
+            self.pasta = caminho
+        # Inicializar dicionário para guardar dados
+        dados = {t: [] for t in self.tipos}
+
+        # Percorrer todos os arquivos da pasta
+        for arquivo in os.listdir(self.pasta):
+            if arquivo.endswith(".txt"):
+                # Extrair o tipo do algoritmo pelo sufixo
+                for t in self.tipos:
+                    if arquivo.endswith(f"{t}.txt"):
+                        caminho = os.path.join(self.pasta, arquivo)
+                        
+                        with open(caminho, "r") as f:
+                            for linha in f:
+                                linha = linha.strip()
+                                if not linha:
+                                    continue
+                                partes = linha.split(" ")
+
+                                tamanho = int(partes[0])
+                                tempo = int(partes[4])
+                                dados[t].append((tamanho, tempo))
+
+        # Transformar em DataFrame para cada tipo e ordenar por tamanho
+        dfs = {}
+        for t in self.tipos:
+            df = pd.DataFrame(dados[t], columns=["Tamanho", "Tempo (μs)"])
+            df = df.sort_values("Tamanho")
+            dfs[t] = df
+
+        # Plotar todos no mesmo gráfico
+        plt.figure(figsize=(10,6))
+        cores = {'i':'blue', 'm':'green', 's':'red'}
+        labels = {'i':'Inserção', 'm':'Merge', 's':'Seleção'}
+
+        for t in self.tipos:
+            plt.plot(dfs[t]["Tamanho"], dfs[t]["Tempo (μs)"], marker='o', linestyle='-', color=cores[t], 
+                    label=labels[t])
+
+        plt.title(titulo)
+        plt.xlabel("Tamanho do Array")
+        plt.ylabel("Tempo de Ordenação (μs)")
+        plt.legend(title="Algoritmo TADS")
+        plt.grid(True)
+        plt.show()
+
+
 # Pasta onde estão os arquivos
-pasta = "008-ordenacao/saidas/caso1/"
+pastas = ["008-ordenacao/saidas/caso1/", "008-ordenacao/saidas/caso2/", 
+          "008-ordenacao/saidas/caso3/", "008-ordenacao/saidas/caso4/",
+          "008-ordenacao/saidas/caso5/"]
 
-# Tipos de ordenação
-tipos = ['i', 'm', 's']
+gg = gera_grafico()
+a = 1
+for caminho in pastas:
+    gg.gerar(caminho, "Comparação de Algoritmos de Ordenação Caso: " + str(a))
+    a += 1
 
-# Inicializar dicionário para guardar dados
-dados = {t: [] for t in tipos}
-
-# Percorrer todos os arquivos da pasta
-for arquivo in os.listdir(pasta):
-    if arquivo.endswith(".txt"):
-        # Extrair o tipo do algoritmo pelo sufixo
-        for t in tipos:
-            if arquivo.endswith(f"{t}.txt"):
-                caminho = os.path.join(pasta, arquivo)
-                
-                with open(caminho, "r") as f:
-                    for linha in f:
-                        linha = linha.strip()
-                        if not linha:
-                            continue
-                        partes = linha.split(" Tempo de ordenação: ")
-                        tamanho = int(partes[0])
-                        tempo = int(partes[1].split(" ")[0])
-                        dados[t].append((tamanho, tempo))
-
-# Transformar em DataFrame para cada tipo e ordenar por tamanho
-dfs = {}
-for t in tipos:
-    df = pd.DataFrame(dados[t], columns=["Tamanho", "Tempo (μs)"])
-    df = df.sort_values("Tamanho")
-    dfs[t] = df
-
-# Plotar todos no mesmo gráfico
-plt.figure(figsize=(10,6))
-cores = {'i':'blue', 'm':'green', 's':'red'}
-
-for t in tipos:
-    plt.plot(dfs[t]["Tamanho"], dfs[t]["Tempo (μs)"], marker='o', linestyle='-', color=cores[t], label=t)
-
-plt.title("Comparação de Algoritmos de Ordenação")
-plt.xlabel("Tamanho do Array")
-plt.ylabel("Tempo de Ordenação (μs)")
-plt.legend(title="Algoritmo")
-plt.grid(True)
-plt.show()
